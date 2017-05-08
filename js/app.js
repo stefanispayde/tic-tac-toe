6ba0656ed65d45b0b177e709d733979475ddc497
stefani spayde <spayde.stefani@gmail.com>
2017-05-08 12:58:42 -0500

    added console log to alert new game after player wins

--- js/app.js
+++ js/app.js
@@ -6,9 +6,7 @@ $(document).ready(function() {
   var currentPlayer = "player1";
   var addMove = 0;
 
-  // alert('Add symbol here.');
 
-  //switch players
   $('.col-xs-4').click(function() {
     if ($(this).hasClass('player1')|| $(this).hasClass('player2')) {
       $('#info').text("You clicked this already");
@@ -20,6 +18,7 @@ $(document).ready(function() {
       addMove++;
     }
 
+    //switch players
     function switchItUp() {
       if(currentPlayer === "player1") {
         currentPlayer = player2;
@@ -35,36 +34,37 @@ $(document).ready(function() {
     if ( $('#1').hasClass('player1') &&  $('#2').hasClass('player1') && $('#3').hasClass('player1') ){
       $('#1').addClass('win') && $('#2').addClass('win') && $('#3').addClass('win');
       console.log('Player 1 is the winner!!');
+      $("#info").text("New game!");
     } else if ( $('#4').hasClass('player1') &&  $('#5').hasClass('player1') && $('#6').hasClass('player1') ){
-        alert('Player 1 is the winner!!');
+        console.log('Player 1 is the winner!!');
     } else if ( $('#7').hasClass('player1') &&  $('#8').hasClass('player1') && $('#9').hasClass('player1') ){
-        alert('Player 1 is the winner!!');
+        console.log('Player 1 is the winner!!');
     } else if ( $('#1').hasClass('player1') &&  $('#4').hasClass('player1') && $('#7').hasClass('player1') ){
-        alert('Player 1 is the winner!!');
+        console.log('Player 1 is the winner!!');
     } else if ( $('#2').hasClass('player1') &&  $('#5').hasClass('player1') && $('#8').hasClass('player1') ){
-        alert('Player 1 is the winner!!');
+        console.log('Player 1 is the winner!!');
     } else if ( $('#3').hasClass('player1') &&  $('#6').hasClass('player1') && $('#9').hasClass('player1') ){
-        alert('Player 1 is the winner!!');
+        console.log('Player 1 is the winner!!');
     } else if ( $('#1').hasClass('player1') &&  $('#5').hasClass('player1') && $('#9').hasClass('player1') ){
-        alert('Player 1 is the winner!!');
+        console.log('Player 1 is the winner!!');
     } else if ( $('#3').hasClass('player1') &&  $('#5').hasClass('player1') && $('#7').hasClass('player1') ){
-        alert('Player 1 is the winner!!');
+        console.log('Player 1 is the winner!!');
     } else if ( $('#4').hasClass('player2') &&  $('#5').hasClass('player2') && $('#6').hasClass('player2') ){
-        alert('Player 2 is the winner!!');
+        console.log('Player 2 is the winner!!');
     } else if ( $('#7').hasClass('player2') &&  $('#8').hasClass('player2') && $('#9').hasClass('player2') ){
-        alert('Player 2 is the winner!!');
+        console.log('Player 2 is the winner!!');
     } else if ( $('#1').hasClass('player2') &&  $('#4').hasClass('player2') && $('#7').hasClass('player2') ){
-        alert('Player 2 is the winner!!');
+        console.log('Player 2 is the winner!!');
     } else if ( $('#2').hasClass('player2') &&  $('#5').hasClass('player2') && $('#8').hasClass('player2') ){
-        alert('Player 2 is the winner!!');
+        console.log('Player 2 is the winner!!');
     } else if ( $('#3').hasClass('player2') &&  $('#6').hasClass('player2') && $('#9').hasClass('player2') ){
-        alert('Player 2 is the winner!!');
+        console.log('Player 2 is the winner!!');
     } else if ( $('#1').hasClass('player2') &&  $('#5').hasClass('player2') && $('#9').hasClass('player2') ){
-        alert('Player 2 is the winner!!');
+        console.log('Player 2 is the winner!!');
     } else if ( $('#3').hasClass('player2') &&  $('#5').hasClass('player2') && $('#7').hasClass('player2') ){
-        alert('Player 2 is the winner!!');
+        console.log('Player 2 is the winner!!');
     } else if (addMove === 8) {
-        alert("It's a draw!");
+        console.log("CAT!");
     }
   };
   //reset button
